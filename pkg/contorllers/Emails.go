d4722b86a722758c7471b7cd6f5db639bf7a71a3
Daniel Idoko <idokoidogwu@gmail.com>
2024-04-02 22:30:46 +0100

    i dn tire

--- pkg/contorllers/Emails.go
+++ pkg/contorllers/Emails.go
@@ -8,7 +8,6 @@ import (
 	"investor-site/pkg/utils"
 	"math/rand"
 	"net/http"
-	"os"
 	"strconv"
 	"strings"
 	"time"
@@ -134,13 +133,13 @@ func HandleChoosePaymentMethodSubmit(w http.ResponseWriter, r *http.Request) {
 		} else {
 
 			m := mail.NewMessage()
-			m.SetHeader("From", os.Getenv("EMAIL"))
+			m.SetHeader("From", "noreply@stargamingstore.shop")
 			m.SetHeader("To", newPayment.Email)
-			m.SetAddressHeader("Cc", os.Getenv("EMAIL"), "Star Gaming Store")
+			m.SetAddressHeader("Cc", "noreply@stargamingstore.shop", "Star Gaming Store")
 			m.SetHeader("Subject", "Payment Confirmation")
 			m.SetBody("text/html", "Hello <b>"+newPayment.Name+"</b> <br></br><br></br><br></br> You have initiated a payment for "+newPayment.ServiceTitle+": <br></br><br></br> Payment Id: <b>"+paymentId+"</b>   <br></br> Package: <b>"+newPayment.PackageName+"</b>   <br></br> Price: <b>$"+strconv.Itoa(newPayment.Price)+".00</b>   <br></br> Delivery Method: <b>"+newPayment.Delivery+"</b> <br></br> Payment Method: <b>"+newPayment.Payment+"</b>   <br></br><br></br>  Use the code below to authenticate payment <br></br> <h1>"+strconv.Itoa(paymentToken)+"</h1> <br></br><br></br> If you didn't initiate this payment, kindly delete this message, Thank you. Star Gaming Store support team.")
 
-			d := mail.NewDialer(os.Getenv("EMAIL_HOST"), 465, os.Getenv("EMAIL"), os.Getenv("APP_PASSWORD"))
+			d := mail.NewDialer("smtp.hostinger.com", 465, "noreply@stargamingstore.shop", "Slowmo29$$$")
 			d.StartTLSPolicy = mail.MandatoryStartTLS
 
 			if err := d.DialAndSend(m); err != nil {
