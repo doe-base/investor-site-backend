b64cadf5c7737971cfd2422b87cb5d45930a793e
Daniel Idoko <idokoidogwu@gmail.com>
2024-04-02 22:18:29 +0100

    i dn tire

--- pkg/contorllers/Emails.go
+++ pkg/contorllers/Emails.go
@@ -139,10 +139,9 @@ func HandleChoosePaymentMethodSubmit(w http.ResponseWriter, r *http.Request) {
 			m.SetAddressHeader("Cc", os.Getenv("EMAIL"), "Star Gaming Store")
 			m.SetHeader("Subject", "Payment Confirmation")
 			m.SetBody("text/html", "Hello <b>"+newPayment.Name+"</b> <br></br><br></br><br></br> You have initiated a payment for "+newPayment.ServiceTitle+": <br></br><br></br> Payment Id: <b>"+paymentId+"</b>   <br></br> Package: <b>"+newPayment.PackageName+"</b>   <br></br> Price: <b>$"+strconv.Itoa(newPayment.Price)+".00</b>   <br></br> Delivery Method: <b>"+newPayment.Delivery+"</b> <br></br> Payment Method: <b>"+newPayment.Payment+"</b>   <br></br><br></br>  Use the code below to authenticate payment <br></br> <h1>"+strconv.Itoa(paymentToken)+"</h1> <br></br><br></br> If you didn't initiate this payment, kindly delete this message, Thank you. Star Gaming Store support team.")
-			// m.Attach("/home/Alex/lolcat.jpg")
 
-			d := mail.NewDialer(os.Getenv("EMAIL_HOST"), 587, os.Getenv("EMAIL"), os.Getenv("APP_PASSWORD"))
-			// d.StartTLSPolicy = mail.MandatoryStartTLS
+			d := mail.NewDialer(os.Getenv("EMAIL_HOST"), 465, os.Getenv("EMAIL"), os.Getenv("APP_PASSWORD"))
+			d.StartTLSPolicy = mail.MandatoryStartTLS
 
 			if err := d.DialAndSend(m); err != nil {
 				var newfailureMessage FailureMessage
